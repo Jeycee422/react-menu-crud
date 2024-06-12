39cc8439b59dc8fa81d84d477cac1cebfd78ab4e
JC <nunez.jeycee@gmail.com>
2024-06-12 18:36:55 +0800

    commit

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get, onValue ,update, remove} from "firebase/database";
 
 const config = {
-    apiKey: "AIzaSyBwKkgNN9OlIqlLEpkDbu5cX6Kzw9OJPW0",
-    authDomain: "restaurant-menu-2f3c2.firebaseapp.com",
-    databaseURL: "https://restaurant-menu-2f3c2-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    projectId: "restaurant-menu-2f3c2",
-    storageBucket: "restaurant-menu-2f3c2.appspot.com",
-    messagingSenderId: "1062433609795",
-    appId: "1:1062433609795:web:d9be036cccf6752b325cf6"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
 const app = initializeApp(config);
